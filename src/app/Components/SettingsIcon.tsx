518af4ad9c6d4f73aaa1ba2876315ef5301cb420
czlabinger <stoffi05@pm.me>
2025-07-22 22:19:18 +0200

    Maybe settings fix

--- src/app/Components/SettingsIcon.tsx
+++ src/app/Components/SettingsIcon.tsx
@@ -1,12 +1,8 @@
 import Image from 'next/image';
 
-const SettingsIcon: React.FC<React.SVGProps<SVGSVGElement>> = () => (
-    <Image
-        src={"/settings.png"}
-        width={30}
-        height={30}
-        alt={"Settings Icon"}
-    />
+const SettingsIcon = () => (
+    <Image src="/images/settings.png" width={30} height={30} alt="Settings Icon" />
+
 );
 
 export default SettingsIcon;
